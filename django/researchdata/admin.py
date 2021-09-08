20a482729ed50720b503defdb4370f492cb585d2
Mike Allaway <m.j.allaway@bham.ac.uk>
2021-09-08 20:07:13 +0100

    changed order of data in admin dashboard

--- django/researchdata/admin.py
+++ django/researchdata/admin.py
@@ -152,7 +152,7 @@ class GenericAdminView(admin.ModelAdmin):
     list_display_links = ('id', 'name')
     list_filter = ('admin_published', 'meta_created_by')
     search_fields = ('name', 'description', 'admin_notes')
-    ordering = ('-id',)
+    ordering = ('name', 'id',)
     actions = (publish, unpublish)
     readonly_fields = ('meta_created_by', 'meta_created_datetime', 'meta_lastupdated_by', 'meta_lastupdated_datetime', 'meta_firstpublished_datetime')
 
@@ -252,6 +252,7 @@ class ReferenceAdminView(GenericAdminView):
     search_fields = ('title', 'subtitle', 'authors_list', 'editors', 'school', 'book_title',
                      'journal_title', 'volume', 'number', 'location', 'year', 'url', 'public_notes', 'admin_notes')
     exclude = ('author', 'linguistic_field', 'linguistic_notion', 'linguistic_tradition')
+    ordering = ('title',)
     filter_horizontal = ('reference',)
     inlines = [AuthorReferenceInline,
                LinguisticFieldReferenceInline,
